9c3074d1c7a4efc43a49e370812bfac4baa07926
Edward Bkheet <bedward@so02.iem.technion.ac.il>
2022-05-17 11:18:21 +0300

    calc mean of means of splits

--- eval.py
+++ eval.py
@@ -2,11 +2,28 @@
 #import sys
 #print(str(sys.argv))
 import argparse
+import numpy as np
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('output_file')
 args = parser.parse_args()
 #print(args.output_file)
 with open(args.output_file, 'r') as f:
-    print(f.read())
+    # print(f.read())
+    lines = f.read().split('\n')[:-1]
+    split_means = []
+    split_stds = []
+
+    for line in lines:
+        if not line:
+            continue
+        tokens = line.split(",")
+        metric = tokens[0]
+        value=tokens[2]
+        if metric == 'mean':
+            split_means.append(value)
+        elif metric == 'std':
+            split_stds.append(value)
+    print(f"mean {np.mean(split_means)}")
+    print(f"std {np.std(split_means)}")
 
